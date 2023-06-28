d38fb20fd84ea7e00207ed783fd795ed66231b51
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-06-28 23:50:33 +0700

    done introduce

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,10 +3,12 @@ import Home from '~/pages/Home';
 import Contact from '~/pages/Contact';
 import Products from '~/pages/Products';
 import ProductDetails from '~/pages/ProductDetails';
+import Introduce from '~/pages/Introduce';
 
 // Public routes
 const publicRoutes = [
-    { path: '/', component: Home },
+    { path: '/', component: Home }, 
+    { path: '/introduce', component: Introduce },
     { path: '/contact', component: Contact },
     { path: '/products', component: Products },
     { path: '/products/:id', component: ProductDetails },
