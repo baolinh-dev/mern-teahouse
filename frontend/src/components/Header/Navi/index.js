ee8eaa371af0e4701b18de16554d3dea0100c73f
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-07-02 03:19:03 +0700

    active link navigation

--- frontend/src/components/Header/Navi/index.js
+++ frontend/src/components/Header/Navi/index.js
@@ -2,7 +2,7 @@ import classNames from 'classnames/bind';
 import styles from './Navi.module.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '~/assets/images/logo.webp';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,30 +10,50 @@ const cx = classNames.bind({ ...styles, container: 'container' });
 const active = cx('active');
 
 function Navigation() {
+    const location = useLocation(); 
+    const isActive = (path) => {
+        if (location.pathname.startsWith('/productCategory')) {
+            return path === '/products' ? active : '';
+        } else {
+            return location.pathname === path ? active : '';
+        }
+    };
     return (
         <div className={cx('navi', 'container')}>
             <div className={cx('navi-left')}>
                 <ul>
                     <li>
-                        <Link className={active} to="/">
+                        <Link className={isActive('/')} to="/">
                             Trang chủ
                         </Link>
                     </li>
                     <li>
-                        <Link to="/menu">Thực đơn</Link>
+                        <Link className={isActive('/menu')} to="/menu">
+                            Thực đơn
+                        </Link>
                     </li>
 
                     <li className={cx('item-product')}>
-                        <Link to="/products">
+                        <Link className={isActive('/products')} to="/products">
                             Sản phẩm
                             <FontAwesomeIcon icon={faCaretDown} />
                         </Link>
                         <div className={cx('dropdown')}>
-                            <Link to="/productCategory?category=Cà phê">Cà phê</Link>
-                            <Link to="/productCategory?category=Bánh ngọt">Bánh ngọt</Link>
-                            <Link to="/productCategory?category=Smoothies">Smoothies</Link>
-                            <Link to="/productCategory?category=Trà hoa quả">Trà hoa quả</Link>
-                            <Link to="/productCategory?category=Trà sữa">Trà sữa</Link>
+                            <Link className={isActive('/productCategory?category=Cà phê')} to="/productCategory?category=Cà phê">
+                                Cà phê
+                            </Link>
+                            <Link className={isActive('/productCategory?category=Bánh ngọt')} to="/productCategory?category=Bánh ngọt">
+                                Bánh ngọt
+                            </Link>
+                            <Link className={isActive('/productCategory?category=Smoothies')} to="/productCategory?category=Smoothies">
+                                Smoothies
+                            </Link>
+                            <Link className={isActive('/productCategory?category=Trà hoa quả')} to="/productCategory?category=Trà hoa quả">
+                                Trà hoa quả
+                            </Link>
+                            <Link className={isActive('/productCategory?category=Trà sữa')} to="/productCategory?category=Trà sữa">
+                                Trà sữa
+                            </Link>
                         </div>
                     </li>
                 </ul>
@@ -44,13 +64,19 @@ function Navigation() {
             <div className={cx('navi-right')}>
                 <ul>
                     <li>
-                        <Link to="/news">Tin tức</Link>
+                        <Link className={isActive('/news')} to="/news">
+                            Tin tức
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/introduce">Giới thiệu</Link>
+                        <Link className={isActive('/introduce')} to="/introduce">
+                            Giới thiệu
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/contact">Liên hệ</Link>
+                        <Link className={isActive('/contact')} to="/contact">
+                            Liên hệ
+                        </Link>
                     </li>
                 </ul>
             </div>
