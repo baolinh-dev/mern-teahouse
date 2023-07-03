1c55fa10393f67c662b3a15d579abaa7e949e3d4
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-07-03 08:16:10 +0700

    fix icon pagination

--- frontend/src/components/Pagination/index.js
+++ frontend/src/components/Pagination/index.js
@@ -1,38 +1,44 @@
 import classNames from 'classnames/bind';
-import styles from './Pagination.module.scss';   
+import styles from './Pagination.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
-const cx = classNames.bind({ ...styles, container: 'container' }); 
+const cx = classNames.bind({ ...styles, container: 'container' });
 
 function Pagination({ currentPage, totalPages, onPageChange }) {
     const pageNumbers = [];
-  
-    if (totalPages > 1) {
-      if (currentPage > 1) {
-        pageNumbers.push(
-          <li key="previous" className={cx('pagi-nav')}>
-            <button onClick={() => onPageChange(currentPage - 1)}>Previous</button>
-          </li>,
-        );
-      }
-  
-      for (let i = 1; i <= totalPages; i++) {
-        pageNumbers.push(
-          <li key={i} className={currentPage === i ? 'active' : null}>
-            <button onClick={() => onPageChange(i)}>{i}</button>
-          </li>,
-        );
-      }
-  
-      if (currentPage < totalPages) {
-        pageNumbers.push(
-          <li key="next" className={cx('pagi-nav')}>
-            <button onClick={() => onPageChange(currentPage + 1)}>Next</button>
-          </li>,
-        );
-      }
-    }
-  
-    return <ul className={cx('pagination')}>{pageNumbers}</ul>;
-  } 
 
-  export default Pagination;
+    if (totalPages > 1) {
+        if (currentPage > 1) {
+            pageNumbers.push(
+                <li key="previous" className={cx('pagi-nav')}>
+                    <button onClick={() => onPageChange(currentPage - 1)}>
+                        <FontAwesomeIcon icon={faAngleLeft} />
+                    </button>
+                </li>,
+            );
+        }
+
+        for (let i = 1; i <= totalPages; i++) {
+            pageNumbers.push(
+                <li key={i} className={currentPage === i ? 'active' : null}>
+                    <button onClick={() => onPageChange(i)}>{i}</button>
+                </li>,
+            );
+        }
+
+        if (currentPage < totalPages) {
+            pageNumbers.push(
+                <li key="next" className={cx('pagi-nav')}>
+                    <button onClick={() => onPageChange(currentPage + 1)}>
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </button>
+                </li>,
+            );
+        }
+    }
+
+    return <ul className={cx('pagination')}>{pageNumbers}</ul>;
+}
+
+export default Pagination;
