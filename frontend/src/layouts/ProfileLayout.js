009254ffda0b5b2aa31a2f2b832af3ee6c6e786e
baolinh <baolinhdev@gmail.com>
2023-08-24 21:36:40 +0700

    add profile layout

---
+++ frontend/src/layouts/ProfileLayout.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Heading from '~/components/Heading';
+import ContainerHeading from '~/components/ContainerHeading';
+import classNames from 'classnames/bind';
+import styles from './ProfileLayout.module.scss';
+import Breadcrumb from '~/components/Breadcrumb';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import MainLayout from '~/layouts/MainLayout';
+import Header from '~/components/Header';
+import Footer from '~/components/Footer';
+
+const cx = classNames.bind({
+    ...styles,
+    container: 'container',
+    'col-xl-6': 'col-xl-6',
+    'col-lg-6': 'col-lg-6',
+    'col-sm-12': 'col-sm-12',
+    'col-xs-12': 'col-xs-12',
+    row: 'row',
+});
+
+function ProfileLayout({ children }) {
+    const [userData, setUserData] = useState(null);
+    const [error, setError] = useState(null);
+
+    const breadcrumbItems = [
+        { label: 'Trang chủ', link: '/' },
+        { label: 'Change Password', link: '/caphe', active: true },
+    ];
+
+    useEffect(() => {
+        axios
+            .get('/api/v1/me')
+            .then((response) => {
+                setUserData(response.data.user);
+            })
+            .catch((error) => {
+                const errorMessage = error.response.data.message;
+                setError(errorMessage);
+            });
+    }, []); 
+
+    // Kiểm tra định dạng hình ảnh
+    const isImageFormat = (url) => {
+        const imageRegex = /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i;
+        return imageRegex.test(url);
+    };
+
+    // Lấy đường dẫn hình ảnh phù hợp
+    const getAvatarUrl = () => {
+        const userAvatar = Cookies.get('userAvatar');
+        if (isImageFormat(userAvatar)) {
+            return userAvatar;
+        } else {
+            return 'https://th.bing.com/th/id/R.b9838bf721d3dff150c954530b3856f3?rik=Uulm6lnhid2Giw&riu=http%3a%2f%2fshackmanlab.org%2fwp-content%2fuploads%2f2013%2f07%2fperson-placeholder.jpg&ehk=GGILj1W77t4L5TSfJq0peMYJY8na6RvFj0vx3uPQHkI%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1';
+        }
+    };
+
+    return (
+        <>
+            <Header />
+            <div className={cx('user-profile', 'container')}>
+                <Breadcrumb items={breadcrumbItems} />
+                <ContainerHeading center>
+                    <Heading content={'Change Password'} />
+                </ContainerHeading>
+                {/* If you have userData, then you are already logged in */}
+                {userData ? (
+                    <div className={cx('user-profile-content')}>
+                        <div className={cx('left-module', 'col-6', 'col-lg-6', 'col-sm-12', 'col-xs-12')}>
+                            <div className={cx('image')}>
+                                <img src={getAvatarUrl()} alt="avatar" />
+                            </div>
+                        </div>
+                        {children}
+                    </div>
+                ) : (
+                    // If you are not logged in, show error message
+                    <div className={cx('error-message')}>{error}</div>
+                )}
+            </div>
+            <Footer />
+            <ToastContainer />
+        </>
+    );
+}
+
+export default ProfileLayout;
--- frontend/src/pages/Profile/ChangePassword/index.js
+++ frontend/src/pages/Profile/ChangePassword/index.js
@@ -11,7 +11,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
-import MainLayout from '~/layouts/MainLayout';
+import ProfileLayout from '~/layouts/ProfileLayout';
 
 const cx = classNames.bind({
     ...styles,
@@ -90,68 +90,51 @@ function ChangePassword() {
 
     return (
         <>
-            <MainLayout>
-                <div className={cx('user-profile', 'container')}>
-                    <Breadcrumb items={breadcrumbItems} />
-                    <ContainerHeading center>
-                        <Heading content={'Change Password'} />
-                    </ContainerHeading>
-                    {/* If you have userData, then you are already logged in */}
-                    {userData ? (
-                        <div className={cx('user-profile-content')}>
-                            <div className={cx('left-module', 'col-6', 'col-lg-6', 'col-sm-12', 'col-xs-12')}>
-                                <div className={cx('image')}>
-                                    <img src={Cookies.get('userAvatar')} alt="avatr" />
-                                </div>
-                            </div>
-                            <div className={cx('right-module', 'col-6', 'col-lg-6', 'col-sm-12', 'col-xs-12')}>
-                                <div className={cx('group-infor')}>
-                                    <b>Old password: </b>
-                                    <input
-                                        placeholder="Vui lòng nhập mật khẩu cũ"
-                                        value={oldPassword}
-                                        onChange={(e) => setOldPassword(e.target.value)}
-                                    />
-                                </div>
-                                <div className={cx('group-infor')}>
-                                    <b>New password: </b>
-                                    <input
-                                        placeholder="Vui lòng nhập mật khẩu mới"
-                                        value={newPassword}
-                                        onChange={(e) => setNewPassword(e.target.value)}
-                                    />
-                                </div>
-                                <div className={cx('group-infor')}>
-                                    <b>Confirm password: </b>
-                                    <input
-                                        placeholder="Vui lòng xác nhận lại mật khẩu mới"
-                                        value={confirmPassword}
-                                        onChange={(e) => setConfirmPassword(e.target.value)}
-                                    />
-                                </div>
-                                <button onClick={handleChangePassword}>Change Password</button>
-                                <div className={cx('buttons')}>
-                                    <div className={cx('button')}>
-                                        <Link to={'/edit-profile'}>Edit profile</Link>
-                                    </div>
-                                    <div className={cx('button')}>
-                                        <Link to={'/user-profile'}>User Profile</Link>
-                                    </div>
-                                    <div className={cx('button')}>
-                                        <Link to={'/my-orders'}>My orders</Link>
-                                    </div>
-                                    <div className={cx('button')}>
-                                        <Link to={'/change-password'}>Change Password</Link>
-                                    </div>
-                                </div>
-                            </div>
+            <ProfileLayout>
+                <div className={cx('right-module', 'col-6', 'col-lg-6', 'col-sm-12', 'col-xs-12')}>
+                    <div className={cx('group-infor')}>
+                        <b>Old password: </b>
+                        <input
+                            placeholder="Vui lòng nhập mật khẩu cũ"
+                            value={oldPassword}
+                            onChange={(e) => setOldPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className={cx('group-infor')}>
+                        <b>New password: </b>
+                        <input
+                            placeholder="Vui lòng nhập mật khẩu mới"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className={cx('group-infor')}>
+                        <b>Confirm password: </b>
+                        <input
+                            placeholder="Vui lòng xác nhận lại mật khẩu mới"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <button onClick={handleChangePassword}>Change Password</button>
+                    </div>
+                    <div className={cx('buttons')}>
+                        <div className={cx('button')}>
+                            <Link to={'/edit-profile'}>Edit profile</Link>
                         </div>
-                    ) : (
-                        // If you are not logged in, show error message
-                        <div className={cx('error-message')}>{error}</div>
-                    )}
+                        <div className={cx('button')}>
+                            <Link to={'/user-profile'}>User Profile</Link>
+                        </div>
+                        <div className={cx('button')}>
+                            <Link to={'/my-orders'}>My orders</Link>
+                        </div>
+                        <div className={cx('button')}>
+                            <Link to={'/change-password'}>Change Password</Link>
+                        </div>
+                    </div>
                 </div>
-            </MainLayout>
+            </ProfileLayout>
             <ToastContainer />
         </>
     );
