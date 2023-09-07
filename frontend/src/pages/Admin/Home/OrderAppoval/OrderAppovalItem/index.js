db5c30afbce19826faa2778a6e155a3579de13e9
baolinh <baolinhdev@gmail.com>
2023-09-08 00:38:37 +0700

    fix css OrderAppovalItem

--- frontend/src/pages/Admin/Home/OrderAppoval/OrderAppovalItem/index.js
+++ frontend/src/pages/Admin/Home/OrderAppoval/OrderAppovalItem/index.js
@@ -7,11 +7,14 @@ const cx = classNames.bind(styles);
 function OrderAppovalItem({ order }) {
     return (
         <div className={cx('order-approval-item')}>
-            <h3>{order.customerInfo.name}</h3>
-            <img alt={order.customerInfo.name} width={200} src={order.customerInfo.avatar} />
+            <div className={cx('infor-customer')}>
+                <img alt={order.customerInfo.name} width={200} src={order.customerInfo.avatar} />
+                <h3>{order.customerInfo.name}</h3>
+            </div>
+
             <p>{order.orderInfo.status}</p>
             <p>{order.dateOrder}</p>
-            <p>
+            <p className={cx('money')}>
                 {order.totalProductPrice.toLocaleString('vi-VN', {
                     style: 'currency',
                     currency: 'VND',
