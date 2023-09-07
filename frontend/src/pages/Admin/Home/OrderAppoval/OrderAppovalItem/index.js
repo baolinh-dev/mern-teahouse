41d06eae7bb672dd3ab96e0facbbbaf221b0aa10
baolinh <baolinhdev@gmail.com>
2023-09-08 00:01:25 +0700

    add unit Vietnam dong

--- frontend/src/pages/Admin/Home/OrderAppoval/OrderAppovalItem/index.js
+++ frontend/src/pages/Admin/Home/OrderAppoval/OrderAppovalItem/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './OrderAppovalItem.module.scss';
 
-const cx = classNames.bind(styles); 
+const cx = classNames.bind(styles);
 
 function OrderAppovalItem({ order }) {
     return (
-        <div className={cx('order-approval-item')}> 
+        <div className={cx('order-approval-item')}>
             <h3>{order.customerInfo.name}</h3>
-            <img alt={order.customerInfo.name} width={200} src={order.customerInfo.avatar}/>
-            <p>{order.orderInfo.status}</p> 
-            <p>{order.dateOrder}</p>  
-            <p>{order.totalProductPrice}</p>
+            <img alt={order.customerInfo.name} width={200} src={order.customerInfo.avatar} />
+            <p>{order.orderInfo.status}</p>
+            <p>{order.dateOrder}</p>
+            <p>
+                {order.totalProductPrice.toLocaleString('vi-VN', {
+                    style: 'currency',
+                    currency: 'VND',
+                })}
+            </p>
         </div>
     );
 }
