38da3cbeefec1d035cd49cadc32a7832209009a4
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-10-15 00:43:46 +0700

    remove search

--- frontend/src/components/Admin/Topbar/index.js
+++ frontend/src/components/Admin/Topbar/index.js
@@ -63,7 +63,7 @@ function Topbar() {
     return (
         <div className={cx('topbar')}>
             <div className={cx('search')}>
-                <input placeholder="Search ..." />
+                <h2>Admin Page</h2>
             </div>
             <div
                 className={cx('info-admin')}
