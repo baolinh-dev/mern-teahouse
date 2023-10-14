e4e954e9b8d38f92278a9c781686a314f11c83eb
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-10-15 00:57:15 +0700

    sidebar active effect

--- frontend/src/components/Admin/Sidebar/CategoryItem/index.js
+++ frontend/src/components/Admin/Sidebar/CategoryItem/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './CategoryItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,15 +5,16 @@ import { Link } from 'react-router-dom';
 
 const cx = classNames.bind({ ...styles, container: 'container' });
 
-function CategoryItem({ categoryName, categoryIcon, categoryUrl }) {
-    return (
-        <Link to={categoryUrl} className={cx('category-item')}>
-            <div className={cx('category-icon')}>
-                <FontAwesomeIcon icon={categoryIcon} />
-            </div>
-            <p>{categoryName}</p>
-        </Link>
-    );
+function CategoryItem({ categoryName, categoryIcon, categoryUrl, isActive }) { 
+    
+  return (
+    <Link to={categoryUrl} className={isActive ? cx('category-item', 'active') : cx('category-item')}>
+      <div className={cx('category-icon')}>
+        <FontAwesomeIcon icon={categoryIcon} />
+      </div>
+      <p>{categoryName}</p>
+    </Link>
+  );
 }
 
 export default CategoryItem;
--- frontend/src/components/Admin/Sidebar/index.js
+++ frontend/src/components/Admin/Sidebar/index.js
@@ -1,29 +1,33 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
 import CategoryItem from './CategoryItem';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
 const cx = classNames.bind({ ...styles, container: 'container' });
 
 function Sidebar({ categories }) {
-    return (
-        <div className={cx('sidebar')}>
-            <div className={cx('logo')}>
-                <Link to={categories[0].url}>
-                    <img src="/static/media/logo.91b6b6a21e219dadbfab.webp" alt="Logo" />
-                </Link>
-            </div>
-            <div className={cx('list-category')}>
-                {categories.map((category, index) => (
-                    <CategoryItem
-                        key={index}
-                        categoryName={category.categoryName}
-                        categoryIcon={category.categoryIcon}
-                        categoryUrl={category.url}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  const location = useLocation();
+
+  return (
+    <div className={cx('sidebar')}>
+      <div className={cx('logo')}>
+        <Link to={categories[0].url}>
+          <img src="/static/media/logo.91b6b6a21e219dadbfab.webp" alt="Logo" />
+        </Link>
+      </div>
+      <div className={cx('list-category')}>
+        {categories.map((category, index) => (
+          <CategoryItem
+            key={index}
+            categoryName={category.categoryName}
+            categoryIcon={category.categoryIcon}
+            categoryUrl={category.url}
+            isActive={location.pathname === category.url}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
