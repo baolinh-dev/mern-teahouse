b0033d2598d993fde3880b2246100c562d712880
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-11-20 08:58:00 +0700

    Remove unnecessary forms on the contact page

--- frontend/src/pages/User/Contact/index.js
+++ frontend/src/pages/User/Contact/index.js
@@ -41,7 +41,7 @@ function Contact() {
                                 title="map"
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3835.7333963918322!2d108.24978007500276!3d15.975293084690664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142108997dc971f%3A0x1295cb3d313469c9!2sVietnam%20-%20Korea%20University%20of%20Information%20and%20Communication%20Technology.!5e0!3m2!1sen!2s!4v1687972021418!5m2!1sen!2s"
                                 width="560"
-                                height="700"
+                                height="320"
                                 style={{ border: 0 }}
                                 allowFullScreen
                                 loading="lazy"
@@ -50,7 +50,7 @@ function Contact() {
                         </div>
                         <div className={cx('infor')}>
                             <ContainerHeading left>
-                                <Heading content={'Gửi thông tin cho chúng tôi'} />
+                                <Heading content={'our contact information'} />
                             </ContainerHeading>
                             <div className={cx('desc')}>
                                 Hãy liên hệ ngay với chúng tôi để nhận được nhiều ưu đãi hấp dẫn dành cho bạn!
@@ -58,29 +58,6 @@ function Contact() {
                             {inforItems.map((item, index) => (
                                 <InforItem key={index} icon={item.icon} label={item.label} text={item.text} />
                             ))}
-
-                            <form>
-                                <div className={cx('form-group')}>
-                                    <input
-                                        type="text"
-                                        id="full-name"
-                                        name="full-name"
-                                        placeholder="Họ và tên*"
-                                        required
-                                    />
-                                </div>
-
-                                <div className={cx('form-group')}>
-                                    <input type="email" id="email" name="email" placeholder="Email*" required />
-                                </div>
-                                <div className={cx('form-group')}>
-                                    <input type="tel" id="phone" name="phone" placeholder="Số điện thoại*" required />
-                                </div>
-                                <div className={cx('form-group')}>
-                                    <textarea id="message" name="message" placeholder="Nội dung"></textarea>
-                                </div>
-                                <button type="submit">Gửi thông tin</button>
-                            </form>
                         </div>
                     </div>
                 </div>
