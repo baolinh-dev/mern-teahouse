347813c77aad2b970130898a0219081a2d98657e
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-12-09 02:39:07 +0700

    remove users in redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'; // Import composeWithDevTools
 import notificationsReducer from './reducers/notificationsReducer';
-import usersReducer from './reducers/usersReducer'; 
 import cartsReducer from './reducers/cartsReducer';
 
 const rootReducer = combineReducers({
   notifications: notificationsReducer,
-  users: usersReducer, 
   carts: cartsReducer
 });
 
